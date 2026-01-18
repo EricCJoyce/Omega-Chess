63b11443054597357d8aa41d5ab94dc17aab58ec
EricCJoyce <50152689+EricCJoyce@users.noreply.github.com>
2026-01-17 22:08:38 -0500

    Add files via upload

---
+++ Move.java
@@ -0,0 +1,13 @@
+public class Move
+  {
+    public int from;
+    public int to;
+    public char promo;
+
+    public Move(int a, int b, char p)
+      {
+        from = a;
+        to = b;
+        promo = p;
+      }
+  }
